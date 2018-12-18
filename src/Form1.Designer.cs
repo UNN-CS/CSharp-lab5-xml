55e32367ce7d19d53d91150dad57f6da7f93733c
yanaslashcheva99 <cool.yanulka2013@yandex.ru>
2018-12-18 17:50:10 +0300

    ready

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(502, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Студенты";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(436, 263);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(602, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(218, 140);
+            this.dataGridView2.TabIndex = 2;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(644, 232);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(381, 123);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1051, 516);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        List<Student> students = new List<Student>();
+        List<Group> groups = new List<Group>();
+        public class Student
+        {
+            public string name { get; set; }// ФИО
+            public int id { get; set; }// номер зачетки
+            public string group { get; set; } // название группы
+            public List<int> marks; // список оценок 2-5
+            public double Average_marks { get; set; } //средняя оценка
+        }
+        public class Group
+        {
+            public string name { get; set; }// Название
+            public List<int> marks = new List<int>();//Список оценок
+            public double avMarks { get; set; }//Средний балл группы
+        }
+       
+
+        int[] marks = new int[] { 2, 3, 4, 5 };//для гистограммы заюзаю
+
+        public Form1()
+        {
+            InitializeComponent();
+            //students = new List<Student>();
+            //groups = new List<Group>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Files XML (*.xml)|*.xml";
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            string filename = dlg.FileName;
+            try
+            {
+                XmlTextReader reader = new XmlTextReader(filename);
+                Student st = null;
+                Group FIIT = new Group();
+                FIIT.name = "381606-3";
+                Group PI1 = new Group();
+                PI1.name = "381607-1";
+                Group PI2 = new Group();
+                PI2.name = "381607-2";
+                Group gr = null;
+                marks[0] = 0;
+                marks[1] = 0;
+                marks[2] = 0;
+                marks[3] = 0;
+                reader.WhitespaceHandling = WhitespaceHandling.None;
+                bool mstart = false;
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        if (reader.Name == "stud")
+                        {
+                            st = new Student();
+                            st.marks = new List<int>();
+                            st.name = reader.GetAttribute("name");
+                            st.id = Convert.ToInt32(reader.GetAttribute("id"));
+                            st.group = reader.GetAttribute("group");
+                            if (st.group == "381606-3") { gr = FIIT; }
+                            else if (st.group == "381607-1") { gr = PI1; }
+                            else if (st.group == "381607-2") { gr = PI2; }
+                        }
+                        else if (reader.Name == "marks")
+                        {
+                            mstart = true;
+                        }
+                        else if (reader.Name == "mark" && mstart)
+                        {
+                            int mark = Convert.ToInt32(reader.GetAttribute("value"));
+                            st.marks.Add(mark);
+                            gr.marks.Add(mark);
+                            if (mark == 2) { marks[0] = marks[0] + 1; }
+                            else if (mark == 3) { marks[1] = marks[1] + 1; }
+                            else if (mark == 4) { marks[2] = marks[2] + 1; }
+                            else if (mark == 5) { marks[3] = marks[3] + 1; }
+                        }
+                     }
+                        else if (reader.NodeType == XmlNodeType.EndElement)
+                        {
+                            if (reader.Name == "marks")
+                            {
+                                students.Add(st);
+                                mstart = false;
+                                double av_m = st.marks.Sum(x => x / 4.0);
+                                st.Average_marks = av_m;
+                            }
+                        }
+                    }
+                    FIIT.avMarks = FIIT.marks.Sum() /(double)FIIT.marks.Count;
+                    PI1.avMarks = PI1.marks.Sum() / (double)PI1.marks.Count;
+                    PI2.avMarks = PI2.marks.Sum() / (double)PI2.marks.Count;
+                    groups.Add(FIIT);
+                    groups.Add(PI1);
+                    groups.Add(PI2);
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+            var source = new BindingSource();
+            source.DataSource = students;
+            dataGridView1.DataSource = source;
+            var source2 = new BindingSource();
+            source2.DataSource = groups;
+            dataGridView2.DataSource = source2;
+            int[] masx = { 2, 3, 4, 5 };
+            this.chart1.Series["Series1"].Points.DataBindXY(masx, marks);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
